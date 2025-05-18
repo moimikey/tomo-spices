968584a267871bc07cbc3414b67037816e0f9184
moimikey <mshertzberg@gmail.com>
2025-05-18 19:01:00 -0400

    feat: add zod and replace raw types
    this sets us up for success for implementing end-to-end type safety

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,20 @@
-export interface Spice {
-  color: string;
-  heat: number;
-  id: number;
-  name: string;
-  price: string;
-}
+import { z } from 'zod';
 
-export interface Blend {
-  blends: number[];
-  description: string;
-  id: number;
-  name: string;
-  spices: number[];
-}
+export const SpiceSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  price: z.string(),
+  color: z.string(),
+  heat: z.number(),
+});
+
+export const BlendSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  blends: z.array(z.number()),
+  spices: z.array(z.number()),
+  description: z.string(),
+});
+
+export type Spice = z.infer<typeof SpiceSchema>;
+export type Blend = z.infer<typeof BlendSchema>;
