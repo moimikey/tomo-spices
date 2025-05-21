4fc6ae743d8e42c63e8f7a11ef2f92d89a5a0ab0
moimikey <mshertzberg@gmail.com>
2025-05-20 21:34:44 -0400

    refactor: clean-up & comments
    adds temporary stylesheet before adding shadcn theme

--- src/router.tsx
+++ src/router.tsx
@@ -1,18 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { prefetchBlends, prefetchSpices } from './queries';
 import Home from './home';
 import BlendDetail from './blend-detail';
 import SpiceDetail from './spice-detail';
-
-export async function homeLoader() {
-  try {
-    await Promise.all([prefetchSpices(), prefetchBlends()]);
-    return null;
-  } catch (error) {
-    console.error('Error in homeLoader:', error);
-    throw error;
-  }
-}
 
 export const routeObjects = [
   {
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 import { z } from 'zod';
 
+/**
+ * Spice
+ */
 export const SpiceSchema = z.object({
   id: z.number(),
   name: z.string(),
@@ -7,7 +10,17 @@ export const SpiceSchema = z.object({
   color: z.string(),
   heat: z.number(),
 });
+export type Spice = z.infer<typeof SpiceSchema>;
 
+/**
+ * Spices
+ */
+export const SpicesSchema = z.array(SpiceSchema);
+export type Spices = z.infer<typeof SpicesSchema>;
+
+/**
+ * Blend
+ */
 export const BlendSchema = z.object({
   id: z.number(),
   name: z.string(),
@@ -15,6 +28,10 @@ export const BlendSchema = z.object({
   spices: z.array(z.number()),
   description: z.string(),
 });
-
-export type Spice = z.infer<typeof SpiceSchema>;
 export type Blend = z.infer<typeof BlendSchema>;
+
+/**
+ * Blends
+ */
+export const BlendsSchema = z.array(BlendSchema);
+export type Blends = z.infer<typeof BlendsSchema>;
